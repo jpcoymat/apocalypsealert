42a2cad790e25175ef50a31d474b76bd6b0c69b5
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-08-23 21:36:52 +0000

    changes

--- app/models/scv_exception.rb
+++ app/models/scv_exception.rb
@@ -1,2 +1,21 @@
 class ScvException < ActiveRecord::Base
+
+  belongs_to: affected_object, polymorphic: true
+  belongs_to: cause_object, polymorphic: true
+
+  enums status: [:open, :closed]
+  
+  validates :type, :priority, :status, :affected_object_id, :affected_object_type, :cause_object_id, :cause_object_type, presence: true
+
+  def self.priorities
+    @@priorities = [1,2,3]
+  end
+
+  def self.types
+    @@types = ["Quantity", "Date"]
+  end
+
+  
+
+
 end
