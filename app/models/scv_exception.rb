aa75cb77a410302b37cede1c519e07cf56af846b
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-08-24 16:27:56 +0000

    changes

--- app/models/scv_exception.rb
+++ app/models/scv_exception.rb
@@ -1,9 +1,9 @@
 class ScvException < ActiveRecord::Base
 
-  belongs_to: affected_object, polymorphic: true
-  belongs_to: cause_object, polymorphic: true
+  belongs_to :affected_object, polymorphic: true
+  belongs_to :cause_object, polymorphic: true
 
-
+  enum status: [ :open, :closed, :archived]
   
   validates :type, :priority, :status, :affected_object_id, :affected_object_type, :cause_object_id, :cause_object_type, presence: true
 
