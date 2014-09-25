46a66a0859327011bf636b22fe2a8fa9bc145e9a
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-09-25 21:47:59 +0000

    changes

--- app/controllers/summary_views_controller.rb
+++ app/controllers/summary_views_controller.rb
@@ -2,11 +2,6 @@ class SummaryViewsController < ApplicationController
 
   before_filter :authorize
 
-  def index
-    @location = Location.where(name: params[:summary_parameters][:location_name]).first    
-    @exceptions = []
-  end
-
   def location_exceptions
     @location = Location.where(code: params[:location_details][:location_code]).first
     @product_categories = User.find(session[:user_id]).organization.product_categories
@@ -20,13 +15,33 @@ class SummaryViewsController < ApplicationController
 
   def location_group_view
     @location_group = LocationGroup.where(name: params[:location_group_summary][:location_group_name]).first
-    @product_categories = User.find(session[:user_id]).organization.product_categories
+    @locations = @location_group.locations
+    @product_categories = @location_group.organization.product_categories
     @exception_category = params[:location_group_summary][:exception_category]
+    @method_to_execute = ""
+    case @exception_category
+      when "Source"
+        @method_to_execute = "source_exception_quantity"
+      when "Make"
+        @method_to_execute = "make_exception_quantity"
+      when "Move"
+        @method_to_execute = "move_exception_quantity"
+      when "Store"
+        @method_to_execute = "store_exception_quantity"
+      when "Deliver"
+        @method_to_execute = "deliver_exception_quantity"
+      when "All"
+        @method_to_execute = "all_exception_quantity"
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def product_category_view
     @product_category = ProductCategory.where(name: params[:product_category_summary][:product_category_name]).first
-    @location_groups = User.find(session[:user_id]).organization.location_groups
+    @location_groups = @product_category.organization.location_groups
     @products = @product_category.products
     @exception_category = params[:product_category_summary][:exception_category]
     @method_to_execute = ""
@@ -41,8 +56,16 @@ class SummaryViewsController < ApplicationController
         @method_to_execute = "store_exception_quantity"
       when "Deliver"
         @method_to_execute = "deliver_exception_quantity"
+      when "All"
+        @method_to_execute = "all_exception_quantity"
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
+
+  
 
 
   protected
